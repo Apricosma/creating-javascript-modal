a659bfa0aaea790a23c9486e85f25f93d3109ebf
Apricosma <113317992+Apricosma@users.noreply.github.com>
2022-11-07 14:10:32 -0600

    add style and scripting

---
+++ assets/script/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// element selector function
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+// button functionality
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+// grabbing the elements of the page
+let modal = select('.modal');
+let openButton = select('.btn');
+let content = select('.content');
+let close = select('.close');
+
+// create button functionality
+onEvent('click', openButton, function() {
+    modal.style.display = 'block'; // changes modal style to visible
+});
+
+onEvent('click', close, function() {
+    modal.style.display = 'none'; // makes the X button close the modal
+})
+
+// closes the modal if you clcik anywhere except the modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
